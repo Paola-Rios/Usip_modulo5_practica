32984eb954e022db3badeb141240ef0a35d3a16b
Paola Rios <paola.rios.grageda@gmail.com>
2022-10-07 15:37:56 -0400

    Model Expensa y Administrador

--- AdminCondominio/adminCondominio/adminExpensa/admin.py
+++ AdminCondominio/adminCondominio/adminExpensa/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Propietario
+from .models import Departamento
+from .models import Expensa_Agua
+from .models import Expensa
 
-# Register your models here.
+
+
+admin.site.register(Propietario)
+admin.site.register(Departamento)
+admin.site.register(Expensa_Agua)
+admin.site.register(Expensa)
+
--- AdminCondominio/adminCondominio/adminExpensa/models.py
+++ AdminCondominio/adminCondominio/adminExpensa/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.conf import settings
+
 class Propietario(models.Model):
     carnet = models.CharField(max_length=100,unique=True) 
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     correo = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return (" Carnet: - %s" % self.carnet)
 
 class Departamento(models.Model):
     departamento_nombre = models.CharField(max_length=100, unique=True)
@@ -11,6 +16,8 @@ class Departamento(models.Model):
     bloque = models.CharField(max_length=100)
     costoExpensa = models.DecimalField(decimal_places=2, max_digits=10)
     fecha_Entrega = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return (" Departamento: - %s" % self.departamento_nombre)
 
 class Expensa_Agua(models.Model):
     Id_Expensa_Agua = models.CharField(max_length=100, unique=True)
@@ -21,13 +28,12 @@ class Expensa_Agua(models.Model):
     anio = models.CharField(max_length=100)
     estado = models.BooleanField(blank=True, default=True)
 
-# class Expensa(models.Model):
-#     Id_Expensa= models.CharField(max_length=100, unique=True)
-#     Departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-#     monto= models.DecimalField(decimal_places=2, max_digits=10)
-#     mes_Expensa= models.CharField(max_length=100)
-#     anio_Expensa= models.CharField(max_length=100)
-#     estado_Expensa = models.Booleanfield(blank=False, default=False)
+class Expensa(models.Model):
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    monto= models.DecimalField(decimal_places=2, max_digits=10)
+    mes_Expensa= models.CharField(max_length=100)
+    anio_Expensa= models.CharField(max_length=100)
+    estado_Expensa = models.BooleanField(blank=False, default=False) 
 
 
 
