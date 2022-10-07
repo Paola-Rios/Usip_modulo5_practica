9ffc528f18f46a46c118ca9acddcec00f2741086
Paola Rios <paola.rios.grageda@gmail.com>
2022-10-07 19:20:30 -0400

    Url and view

--- AdminCondominio/adminCondominio/adminExpensa/urls.py
+++ AdminCondominio/adminCondominio/adminExpensa/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
+    path('contact/<str:name>', views.contact, name = 'contact'),
     path ("", views.index, name = 'index'),
+    path('propietario/', views.propietario, name = 'propietario')
 ]
--- AdminCondominio/adminCondominio/adminExpensa/views.py
+++ AdminCondominio/adminCondominio/adminExpensa/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Propietario
 
 # function view 
 def index (request):
     return HttpResponse("hola mundo")
+
+def contact(request, name):
+    return HttpResponse(f"Bienvenido {name} a la clase de django")
+def propietario(request):
+#     post_carnet = request.POST.get('carnet')
+#     post_nombre = request.POST.get('nombre')
+#     post_apellido = request.POST.get('apellido')
+#     post_correo = request.POST.get('correo')
+#    # if post_carnet:
+#         q= Propietario(carnet=post_carnet, nombre=post_nombre, apellido=post_apellido, correo=post_correo)
+#         q.save()
+
+    propietarios = Propietario.objects.all()
+    return render(request,"propietario.html", {'propietarios': propietarios})
